98fe52c7e08d19a0252de3d0bdcff40149a2818c
vinay techdev <vinayvinuu629@gmail.com>
2025-09-30 23:01:34 +0530

    feat: add chapter 02 Learning/content

---
+++ basicMath/Prime.java
@@ -0,0 +1,30 @@
+package basicMath;
+
+import java.util.Scanner;
+
+public class Prime {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Number to check Prime or not");
+		int num = sc.nextInt();
+		
+		int count = 0;
+		for(int i = 1; i*i<num; i++) {
+			if(num%i == 0) {
+				count++;
+				
+				if ((num/i) != i) count++;
+			}
+		}
+		if(count == 2) {
+			System.out.println("Number is Prime");
+		}else {
+			System.out.println("Number is not Prime");
+		}
+		sc.close();
+
+	}
+
+}
