74b0ed1c73e56b3a21cac2e648ab772b251b2ad1
vinay techdev <vinayvinuu629@gmail.com>
2025-10-01 18:44:22 +0530

    feat: add chapter 02 Learning/content

---
+++ _02_basicMath/Armstrong.java
@@ -0,0 +1,31 @@
+package _02_basicMath;
+
+import java.util.Scanner;
+
+public class Armstrong {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Number to check palindrome or not");
+		int num = sc.nextInt();
+	
+		
+		int number = num;
+		int sum = 0;
+		while(num>0){
+			int lastdigit = num%10;
+			sum = sum + (lastdigit*lastdigit*lastdigit);
+			num = num/10;
+		}
+		
+		if (number == sum) {
+			System.out.println("Number is Armstrong");
+		} else {
+			System.out.println("Number is not Armstrong");
+		}
+		sc.close();
+
+	}
+
+}
---
+++ _02_basicMath/GCD.java
@@ -0,0 +1,30 @@
+package _02_basicMath;
+
+import java.util.Scanner;
+
+public class GCD {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First Number");
+		int m = sc.nextInt();
+		
+		System.out.println("Enter Second Number");
+		int n = sc.nextInt();
+		
+		
+		while(n != 0) {
+			
+			int rem = m%n;
+			m=n;
+			n=rem;
+		}
+		
+		System.out.println(m);
+		
+		sc.close();
+
+	}
+
+}
